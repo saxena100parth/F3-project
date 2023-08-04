75012fd4effbbbb2931867a7d9f2821fa9b3703a
saxena100parth <87128985+saxena100parth@users.noreply.github.com>
2023-08-05 01:41:58 +0530

    updated

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-// myProducts.filter((item)=>item.title.includes(search.value))
+let login = document.getElementById("login-btn");
+let signup = document.getElementById("Signup-btn");
 
-// myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
+login.addEventListener("click", () => {
+  window.location.href = "./login/index.html";
+});
+signup.addEventListener("click", () => {
+  window.location.href = "./signup/index.html";
+});
+
+if (sessionStorage.getItem("currentuser")) {
+  alert("you have already logged in");
+  setTimeout(() => {
+    window.location.href = "./shop/index.html";
+  }, 1000);
+}
+
+document.getElementById("home").addEventListener("click", (e) => {
+  e.preventDefault();
+  if (sessionStorage.getItem("currentuser")) {
+    window.location.href = "../shop/index.html";
+  } else {
+    alert("Please login first");
+  }
+});
